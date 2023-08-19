e2b9c4cb6bcc4785ff145749a4b1522c72450f62
vaidehi2701 <vaidehi2701@gmail.com>
2023-08-19 14:34:42 +0530

    press save for more

--- lib/modules/players_page.dart
+++ lib/modules/players_page.dart
@@ -19,135 +19,147 @@ class PlayerPage extends StatelessWidget {
       () => agentsController.isLoading.value
           ? const Center(child: CircularProgressIndicator())
           : Stack(children: [
-              LiquidSwipe.builder(
-                itemCount: agentsController.allAgents.length,
-                liquidController: agentsController.liquidController,
-                ignoreUserGestureWhileAnimating: true,
-                itemBuilder: (context, index) {
-                  return Container(
-                    padding: const EdgeInsets.all(16),
-                    width: double.maxFinite,
-                    height: double.maxFinite,
-                    color: agentsController.bgColors[index],
-                    child: Column(
-                      children: [
-                        const SizedBox(
-                          height: 32,
-                        ),
-                        Text(
-                          "${agentsController.allAgents[index].displayName?.toUpperCase()}",
-                          style: const TextStyle(
-                              color: ThemeColor.white,
-                              fontSize: 32,
-                              fontWeight: FontWeight.bold),
-                        ),
-                        const SizedBox(
-                          height: 16,
-                        ),
-                        Expanded(
-                            child: Stack(children: [
-                        
-                          Opacity(
-                            opacity: 0.5,
-                            child: CachedNetworkImage(
-                                width: MediaQuery.of(context).size.width * 1.2,
-                                height: MediaQuery.of(context).size.width * 1.2,
-                                imageUrl: agentsController
-                                        .allAgents[index].background ??
-                                    ""),
-                          ),
-                          CachedNetworkImage(
-                              width: double.infinity,
-                              height: double.infinity,
-                              fit: BoxFit.cover,
-                              imageUrl: agentsController
-                                      .allAgents[index].fullPortrait ??
-                                  ""),
-                        ])),
-                        const SizedBox(
-                          height: 100,
-                        ),
-                      ],
-                    ),
-                  );
-                },
-                onPageChangeCallback: (value) {
-                  agentsController.activeIndex.value = value;
-                  agentsController.tabController?.animateTo(
-                    value,
-                    duration: const Duration(milliseconds: 500),
-                    curve: Curves.ease,
-                  );
-                },
-              ),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.end,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Container(
-                    alignment: Alignment.centerLeft,
-                    child: DefaultTabController(
-                        initialIndex: agentsController.activeIndex.value,
-                        length: agentsController.allAgents.length,
-                        child: Column(
-                          children: [
-                            ButtonsTabBar(
-                              controller: agentsController.tabController,
-                              radius: 8,
-                              height: 80,
-                              backgroundColor: Colors.transparent,
-                              unselectedBackgroundColor: Colors.transparent,
-                              borderColor: ThemeColor.red,
-                              borderWidth: 4,
-                              unselectedBorderColor: Colors.transparent,
-                              buttonMargin:
-                                  const EdgeInsets.symmetric(horizontal: 28),
-                              tabs: agentsController.allAgents
-                                  .mapIndexed((pos, item) {
-                                return Tab(
-                                  icon: Container(
-                                    decoration: BoxDecoration(
-                                        borderRadius: BorderRadius.circular(8),
-                                        color: ThemeColor.black),
-                                    width: 60,
-                                    height:
-                                        agentsController.activeIndex.value ==
-                                                pos
-                                            ? 80
-                                            : 64,
-                                    child: agentsController.activeIndex.value ==
-                                            pos
-                                        ? CachedNetworkImage(
-                                            imageUrl: item.displayIcon ?? "")
-                                        : Opacity(
-                                            opacity: 0.5,
-                                            child: CachedNetworkImage(
-                                                imageUrl:
-                                                    item.displayIcon ?? ""),
-                                          ),
-                                  ),
-                                );
-                              }).toList(),
-                              onTap: (index) {
-                                agentsController.activeIndex.value = index;
-                                agentsController.tabController?.animateTo(
-                                  index,
-                                  duration: const Duration(milliseconds: 500),
-                                  curve: Curves.ease,
-                                );
-                                agentsController.liquidController
-                                    .jumpToPage(page: index);
-                              },
-                            ),
-                          ],
-                        )),
-                  ),
-                  const SizedBox(
-                    height: 16,
-                  ),
-                ],
-              ),
+              Swiper(agentsController: agentsController),
+              MultipleTabs(
+                agentsController: agentsController,
+              )
             ]),
     ));
+  }
+}
+
+class Swiper extends StatelessWidget {
+  final PlayersController agentsController;
+  const Swiper({super.key, required this.agentsController});
+
+  @override
+  Widget build(BuildContext context) {
+    return LiquidSwipe.builder(
+      itemCount: agentsController.allAgents.length,
+      liquidController: agentsController.liquidController,
+      ignoreUserGestureWhileAnimating: true,
+      itemBuilder: (context, index) {
+        return Container(
+          padding: const EdgeInsets.all(16),
+          width: double.maxFinite,
+          height: double.maxFinite,
+          color: agentsController.bgColors[index],
+          child: Column(
+            children: [
+              const SizedBox(
+                height: 32,
+              ),
+              Text(
+                "${agentsController.allAgents[index].displayName?.toUpperCase()}",
+                style: const TextStyle(
+                    color: ThemeColor.white,
+                    fontSize: 32,
+                    fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+              Expanded(
+                  child: Stack(children: [
+                Opacity(
+                  opacity: 0.5,
+                  child: CachedNetworkImage(
+                      width: MediaQuery.of(context).size.width * 1.2,
+                      height: MediaQuery.of(context).size.width * 1.2,
+                      imageUrl:
+                          agentsController.allAgents[index].background ?? ""),
+                ),
+                CachedNetworkImage(
+                    width: double.infinity,
+                    height: double.infinity,
+                    fit: BoxFit.cover,
+                    imageUrl:
+                        agentsController.allAgents[index].fullPortrait ?? ""),
+              ])),
+              const SizedBox(
+                height: 100,
+              ),
+            ],
+          ),
+        );
+      },
+      onPageChangeCallback: (value) {
+        agentsController.activeIndex.value = value;
+        agentsController.tabController?.animateTo(
+          value,
+          duration: const Duration(milliseconds: 500),
+          curve: Curves.ease,
+        );
+      },
+    );
+  }
+}
+
+class MultipleTabs extends StatelessWidget {
+  final PlayersController agentsController;
+  const MultipleTabs({super.key, required this.agentsController});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.end,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Container(
+          alignment: Alignment.centerLeft,
+          child: DefaultTabController(
+              initialIndex: agentsController.activeIndex.value,
+              length: agentsController.allAgents.length,
+              child: Column(
+                children: [
+                  ButtonsTabBar(
+                    controller: agentsController.tabController,
+                    radius: 8,
+                    height: 80,
+                    backgroundColor: Colors.transparent,
+                    unselectedBackgroundColor: Colors.transparent,
+                    borderColor: ThemeColor.red,
+                    borderWidth: 4,
+                    unselectedBorderColor: Colors.transparent,
+                    buttonMargin: const EdgeInsets.symmetric(horizontal: 28),
+                    tabs: agentsController.allAgents.mapIndexed((pos, item) {
+                      return Tab(
+                        icon: Container(
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(8),
+                              color: ThemeColor.black),
+                          width: 60,
+                          height: agentsController.activeIndex.value == pos
+                              ? 80
+                              : 64,
+                          child: agentsController.activeIndex.value == pos
+                              ? CachedNetworkImage(
+                                  imageUrl: item.displayIcon ?? "")
+                              : Opacity(
+                                  opacity: 0.5,
+                                  child: CachedNetworkImage(
+                                      imageUrl: item.displayIcon ?? ""),
+                                ),
+                        ),
+                      );
+                    }).toList(),
+                    onTap: (index) {
+                      agentsController.activeIndex.value = index;
+                      agentsController.tabController?.animateTo(
+                        index,
+                        duration: const Duration(milliseconds: 500),
+                        curve: Curves.ease,
+                      );
+                      agentsController.liquidController.jumpToPage(page: index);
+                    },
+                  ),
+                ],
+              )),
+        ),
+        const SizedBox(
+          height: 16,
+        ),
+      ],
+    );
   }
 }
